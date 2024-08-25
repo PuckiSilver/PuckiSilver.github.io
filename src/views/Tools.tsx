c1323c7fc185513992df456ad1feeecff4b0d00a
PuckiSilver <pucki.silver@gmail.com>
2024-08-25 09:09:42 +0200

    add external link to my tools on github

--- src/views/Tools.tsx
+++ src/views/Tools.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import './Tools.scss'
+import LinkCard from '../components/LinkCard'
+import BoxIcon from '../icons/box'
+import ShieldIcon from '../icons/shield'
 
 const Tools = () => {
     return (
         <main className='tools'>
             <h1>Tools</h1>
             <div className='section'>
-                <p>
-                    Tools and utilities that I create(d) will be provided through this website.
-                </p>
+                <h2>Scripts on GitHub</h2>
+                <div className='grid'>
+                    <LinkCard
+                        title='Cardboard'
+                        description='A tool to package minecraft data packs and resource packs to mods for various mod loaders'
+                        link='https://github.com/PuckiSilver/Cardboard'
+                        icon={<BoxIcon />}
+                    />
+                    <LinkCard
+                        title='FancyPantsLite▶CIT'
+                        description='A tool to convert minecraft vanilla custom armor to be used with mods'
+                        link='https://github.com/PuckiSilver/FancyPantsLite-to-CIT'
+                        icon={<ShieldIcon />}
+                    />
+                </div>
             </div>
         </main>
     )
